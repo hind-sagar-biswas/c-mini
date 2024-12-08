dbb17737593148643eab4afadf9bcba8dc5e5bcd
Hind Sagar Biswas <hindsbhk@gmail.com>
2024-12-08 13:48:04 +0000

    minor update

--- w3resources/basic_dec/2302016_60.c
+++ w3resources/basic_dec/2302016_60.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 int main() {
 	char days[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-	for (int i = 0; i < 7; i++)
-	{
-		printf("%s = %d\n", days[i], i);
-	}
+	for (int i = 0; i < 7; i++) printf("%s = %d\n", days[i], i);
 	return 0;
 }
