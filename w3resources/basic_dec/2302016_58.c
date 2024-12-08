e1e4cba2f237cf054803065cc5d44206ba4eda9c
Hind Sagar Biswas <hindsbhk@gmail.com>
2024-12-08 13:24:04 +0000

    no. 58

---
+++ w3resources/basic_dec/2302016_58.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+	float min, max, current;
+	for (int i = 0; i < 4; i++)
+	{
+		scanf("%f", &current);
+		if (!i) {
+			min = current;
+			max = current;
+		}
+		else if (current < min) min = current;
+		else if (current > max) max = current;
+	}
+
+	printf("%f\n", max - min);
+	return 0;
+}
