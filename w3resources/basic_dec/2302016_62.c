fd793829075c85ab54de6a7bd144f0ceedda96e9
Hind Sagar Biswas <hindsbhk@gmail.com>
2024-12-09 15:40:32 +0600

    Create 2302016_62.c

---
+++ w3resources/basic_dec/2302016_62.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    unsigned short int x, sum = 0;
+    scanf("%hu", &x);
+    if (x < 500) {
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        printf("Sum of the digits of %hu: %hu", x, sum);
+    }
+    else printf("Input must be less than 0");
+    return 0;
+}
