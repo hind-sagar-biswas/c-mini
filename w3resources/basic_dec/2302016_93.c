93534daca71f2e6f0ed3fae6653b757d586a5302
Hind Sagar Biswas <hindsbhk@gmail.com>
2024-12-13 03:25:57 +0600

    Bug Fix

--- w3resources/basic_dec/2302016_93.c
+++ w3resources/basic_dec/2302016_93.c
@@ -5,11 +5,12 @@
 int primes[NUM_OF_PRIMES] = {2, 3, 5, 7}, num_of_primes = 4;
 
 void setup();
-bool is_prime(int num);
 bool is_nearly_prime(int num);
+bool is_prime(int num, bool generative);
 
 int main() {
     setup();
+    printf("Enter a number: ");
     int x;
     scanf("%d", &x);
     if (is_nearly_prime(x)) printf("It is a Nearly prime number.\n");
@@ -18,22 +19,24 @@ int main() {
 }
 
 void setup() {
-	for (int i = 0; i < 1000000000; i++)
+	for (int i = 8; (i < 1000000000) && (num_of_primes < NUM_OF_PRIMES); i++)
 	{
-		if (is_prime(i)) primes[num_of_primes++] = i;
+		if (is_prime(i, true)) primes[num_of_primes++] = i;
 	}
 }
-bool is_prime(int num) {
+bool is_prime(int num, bool generative) {
+    if (num < 2) return false;
     for (int i = 0; i < num_of_primes; i++) {
-        if (num % i == 0) return false;
+        if (generative && num % primes[i] == 0) return false;
+	else if (!generative && num == primes[i]) return true;
     }
-    return true;
+    return generative ? true : false;
 }
 
 bool is_nearly_prime(int num) {
     for (int i = 0; (i < num_of_primes) && primes[i] * primes[i] <= num; i++) 
     {
-        if (num % primes[i] == 0 && is_prime(num / primes[i])) return false;
+        if (num % primes[i] == 0 && is_prime(num / primes[i], false)) return true;
     }
-    return true;    
+    return false;
 }
