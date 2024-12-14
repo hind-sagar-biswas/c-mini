416b9be825e5a447fc7c5ac97b9a002e45e417f0
Hind Sagar Biswas <hindsbhk@gmail.com>
2024-12-14 16:19:48 +0600

    Minor update

--- w3resources/basic_dec/2302016_64.c
+++ w3resources/basic_dec/2302016_64.c
@@ -5,19 +5,19 @@ int main() {
 	while (x > 0) {
 		scanf("%d", &x);
 		if (min == -1) {
-			if (x < 0) return printf("Input must be a positive integer!");
+			if (x <= 0) return printf("Input must be a positive integer!");
 			min = x;
 			max = x;
 			sum = x;
 		}
 		else {
-			if (x < 0) break;
+			if (x <= 0) break;
 			sum += x;
 			max = x > max ? x : max;
 			min = x < min ? x : min;
 		}
 		ctr++;
 	}
-	printf("Max: %d\nMin: %d\nAverage: %.2f\n", max, min, (float) sum / ctr);
+	printf("Total Entered: %d\nMax: %d\nMin: %d\nAverage: %.2f\n", ctr, max, min, (float) sum / ctr);
 	return 0;
 }
