94e2fba45b46a9877dafc0ca44e4a5e00732226a
Hind Sagar Biswas <hindsbhk@gmail.com>
2024-12-22 23:28:18 +0600

    Bug fix

--- w3resources/basic_dec/2302016_30.c
+++ w3resources/basic_dec/2302016_30.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-	int n;
-	scanf("%d", &n);
-	for (n = 0; n < 5; n++) {
+	int n, i;
+	scanf("%d", &i);
+	for (n = 1; n <= i; n++) {
 		if (n % 2 == 0) printf("%d^2 = %d\n", n, n * n);
 	}
 	return 0;
